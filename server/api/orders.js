185a63d249181450a0fa775d8cf03ea67965aca3
Ayman-cds <business.aymanadil@gmail.com>
2021-04-22 18:52:47 +0300

    adds orders routes

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,6 +1,11 @@
 
-
+const router = require('express').Router();
+const {
+  models: { Order },
+} = require('../db');
+module.exports = router;
 // get route for all orders ...
+
 
 // get route for one person (e.g. to see one person's cart)
 // get/orders/id
@@ -8,3 +13,16 @@
 // post route to create an order ....
 
 // put route for updating quantity based on user id/item id ...
+
+
+// put routes to change the active status of an order
+
+router.put('/checkout/:orderId', (req,res,next)=>{
+  try {
+    const order = await Order.findByPk(orderId)
+    const update = await order.update({"active": false})
+    res.send(update)
+  } catch (error) {
+    console.log(error)
+  }
+})
